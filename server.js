462a612cdd66ed1e53ca3b57f95235142e49ea32
BanceChattles <cb0885404@otc.edu>
2019-04-08 12:06:16 -0500

    Adjusted files

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
 
 app.set('view engine', 'pug');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
 });
 
-
+app.get('/profile', (req, res) => {
+    res.send(req.query.id);
+  });
 
 const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+  console.log(`Express running → PORT ${server.address().port}`);
 });
-
