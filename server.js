99aa91a51f54dc07b002e94b59f689d0ec24200d
BanceChattles <cb0885404@otc.edu>
2019-04-08 12:49:50 -0500

    added index.pug

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-  });
+});
+
+app.set('view engine', 'pug');
 
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
